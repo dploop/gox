abd97179dd624db1613c3502b59684223785aae3
Qiang Liu <dploop@163.com>
2021-01-18 10:36:40 +0800

    baseline

--- baseline/memory/main.go
+++ baseline/memory/main.go
@@ -6,17 +6,24 @@ import (
 )
 
 const (
-	repeat = 10000000000
+	repeat = 100
+	length = 100000000
 )
 
 func main() {
+	array := make([]int, length)
+	for i := 0; i < length; i++ {
+		array[i] = i
+	}
 	start := time.Now()
 	var sum int64
 	for r := 0; r < repeat; r++ {
-		sum += int64(r & 1)
+		for i := 0; i < length; i++ {
+			sum += int64(array[i])
+		}
 	}
 	elapsed := time.Since(start)
 	log.Printf("sum is %v", sum)
-	ops := float64(repeat) / elapsed.Seconds()
+	ops := float64(length) / elapsed.Seconds()
 	log.Printf("%.0f operation per second", ops)
 }
